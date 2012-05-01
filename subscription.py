4d7ab49d6fd6360f56996b085f35803a7b460280
justicezyx <lovelybear.zhao@gmail.com>
2012-04-30 23:08:45 -0400

    revised many details
    protocol is revised:
    1. NAMEREQ -> NREQ
    2. state machine rules revised
    
    peer behavrios changed, now each server first listen on a designated
    port, then tries to connect alll peers in its peer list. Connect fails
    with a timeout and can retry upto a limit that is specified by
    parameters
    
    subscription format checking
    invalid if has less than 4 or more than 5 fields

--- subscription.py
+++ subscription.py
@@ -1,44 +1,72 @@
-import re
+"""
+Attribute value types
+"""
 
 INT = 'INTEGER'
 DOU = 'DOUBLE'
 STR = 'STRING'
+
+"""
+Operators
+"""
 
 IN = 'in'
 LT = '<'
 GT = '>'
 LE = '<='
 GE = '>='
+EQ = '='
 
+def FormatCheck(string):
+    """
+    Subscription format check
+    """
+
+    for c in string.split('}{'):
+        cons = c.split(',')
+        if len(cons) < 4 or len(cons) > 5:
+            return False
+        
+        for con in cons:
+            if len(con) == 0:
+                return False
+    return True
+
+def ParseAttributeAssignments(string):
+    """
+    Attribute assignments have format as follows:
+    [[attribute name]=[attribute type]:[value]],[other attribute assignment]
+    """
+
+    vals = string.split(',')
+    res = {}
+    for v in vals:
+        name,value = v.split("=")
+        valueType, valueString = value.split(":")
+        val = CreateType(valueType).Parse(valueString)
+        res[name] = val
+    return res
 
 class Subscription:
+    """
+    Subcription will have the format as follows:
+    {[attribute value type],[attribute name],[operator],[value]}{[other constraints]}
+    """
+
     def __init__(self, data):
         self.attrConstraints = {}
-
         for constraint in data.split('}{'):
             constraint = constraint.strip('{}')
             Type,Name,Op,Val = constraint.split(',', 3)
-            
             self.attrConstraints[Name] = Constraint(Type, Op, Val)
-
         self.count = len(self.attrConstraints)
 
-    # val argument is assumed to be a comma-separated string
-    # no white space is allowed. each field, which is separated by a comma,
-    # is an attribute assignment of the form "attribute name"="type:value"
-    def Match(self, val):
-        vals = val.split(",")
-        if len(vals) < self.count:
-            return False
+    def Match(self, assignments):
+        """
+        Attribute assignments have format as follows:
+        [attribute name]=[attribute type]:[value],[other attribute assignment]
+        """
 
-        assignments = {}
-        for v in vals:
-            name,assignment = v.split("=")
-            
-            assignmentType, assignmentString = assignment.split(":")
-            val = CreateType(assignmentType).Parse(assignmentString)
-            assignments[name] = val
-            
         for name,constraint in self.attrConstraints.items():
             if not assignments.has_key(name):
                 return False
@@ -47,22 +75,13 @@ class Subscription:
 
         return True
             
-    #def Match(self, **val):
-        #if len(val) < self.count:
-            #return False
-
-        #for name,constraint in self.attrConstraints.items():
-            #if not val.has_key(name): # has no value for the attr
-                #return False
-
-            #if not constraint.Match(val[name]):
-                #return False
-
-        #return True
-
 class OpIN:
     def Check(self, cons, val):
         return val > cons[0] and val < cons[1]
+
+class OpEQ:
+    def Check(self, cons, val):
+        return val == cons
 
 class OpGT:
     def Check(self, cons, val):
@@ -96,6 +115,9 @@ def CreateOperator(type):
     if type == LE:
         return OpLE()
 
+    if type == EQ:
+        return OpEQ()
+
 class TypeInteger:
     def Parse(self, val):
         return int(val)
@@ -120,15 +142,17 @@ def CreateType(tp):
     
 class Constraint:
     def __init__(self, tp, op, val):
-        self.type = CreateType(tp)
         self.optr = CreateOperator(op)
-        self.value = val
+        if op == IN:
+            vals = val.split(',')
+            self.value = (CreateType(tp).Parse(vals[0]), CreateType(tp).Parse(vals[1]))
+        else:
+            self.value = CreateType(tp).Parse(val)
 
     def Match(self, val):
-        v = self.type.Parse(val)
-        return self.optr.Check(self.value, v)
+        return self.optr.Check(self.value, val)
         
 if __name__ == '__main__':
-    sub = Subscription('{INTEGER,age,<,100}{INTEGER,height,<=,200}')
-    if sub.Match("age=INTEGER:10,height=INTEGER:10"):
+    sub = Subscription('{INTEGER,age,>,1}')
+    if sub.Match(ParseAttributeAssignments("age=INTEGER:2,height=INTEGER:2")):
         print "correct"
