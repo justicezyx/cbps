d19483a7c4f186b9f1d8aead178641fa3b6fe4cd
Yaxiong Zhao <justicezyx@gmail.com>
2012-05-04 15:38:00 -0400

    Added basic test
    Send three messages to the peer manager

--- peer_test.py
+++ peer_test.py
@@ -8,16 +8,53 @@ Usage:
     2. Run this file
 """
 
-class DummyPeer(protocol.Protocol):
+class DummyPeerConnection(protocol.Protocol):
     """ DummyPeer for testing
 
     Function identically as PeerManager
     Considering inherent from PeerManager
     """
-    
 
-class DummyPeerFactory(protocol.ClientFactory):
-    protocol = DummyPeer
+    def __init__(self):
+        self.dataQueue = ['NAME,zyx',
+                          'SUB,{INTEGER,age,>,1}',
+                          'MSG,age=INTEGER:2']
+        self.currentIndex = 0
+
+    def connectionMade(self):
+        Log.Msg("Connection made")
+        Log.Msg('Queue length', str(len(self.dataQueue)) )
+        self.localHostName = self.factory.localHostName
+
+    def SendData(self):
+        Log.Msg('SendData index', str(self.currentIndex))
+
+        if self.currentIndex >= 3:
+            return
+
+        self.transport.write(self.dataQueue[self.currentIndex])
+        self.currentIndex += 1
+
+        if self.currentIndex < 3:
+            reactor.callLater(1, self.SendData)
+
+    def dataReceived(self, data):
+        Log.Data(data)
+        reactor.callLater(1, self.SendData)
+
+class DummyPeerConnectionFactory(protocol.ClientFactory):
+    protocol = DummyPeerConnection
+
+    def __init__(self, name):
+        self.localHostName = name
+
+from twisted.internet import reactor
+import sys
 
 if __name__ == '__main__':
-    pass
+    Log.StartLogging(sys.stdout)
+
+    factory = DummyPeerConnectionFactory('zyx')
+    reactor.connectTCP('localhost', 10000, factory)
+
+    reactor.run()
