1e6b1f891dd74cd7a24db0ae5008155286cc5bae
Yaxiong Zhao <justicezyx@gmail.com>
2012-05-09 15:05:15 -0400

    Revised client and manager

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 from twisted.internet import reactor, protocol
 from util import Log
+from util import NetInfo
 import config
 import sys
 
@@ -20,25 +21,62 @@ class Client(protocol.Protocol):
         The listening port of the client manager is 10001
         """
         self.username = self.factory.username
+        self.factory.connection = self
+
+    def Send(self, data):
+        self.transport.write(data)
         
     def dataReceived(self, data):
         """ Received data
-
         The data should comply with the protocol
         """
-        
-        if data.split(',', 1)[0] == 'NREQ':
-            self.transport.write('NAME,' + self.username)
+
+        if not ',' in data:
+            cmd, val = data, ''
+        else:
+            cmd, val = data.split(',', 1)
+
+        if cmd == 'NREQ':
+            self.Send('NAME,' + self.username)
+            return
+
+    def connectionLost(self, reason):
+        # after lost connection set the connection to None
+        self.factory.connection = None
     
 class ClientFactory(protocol.ClientFactory):
     protocol = Client
 
-    def __init__(self, username):
+    def __init__(self, username, remote_name):
         self.username = username
+        self.remoteHostName = remote_name
 
+        self.retryCount = 0
+        self.retryLimit = 3
+
+        self.connection = None
+
+    def clientConnectionFailed(self, connector, reason):
+        self.retryCount += 1
+        Log.Err('[Connection failed]', connector.remoteHostName,
+                '[reason]', reason.getErrorMessage(),
+                '[retry]', str(self.retryCount))
+
+        if self.retryCount < self.retryLimit:
+            connector.connect()
+
+def Connect(remote_host = 'localhost', remote_port = config.BR_CLIENT_LISTEN_PORT):
+    factory = ClientFactory(NetInfo.GetLocalDomainName(), remote_host)
+    connector = reactor.connectTCP(factory.remoteHostName, remote_port, factory)
+    connector.remoteHostName = factory.remoteHostName
+    return factory
+    
+    
 if __name__ == '__main__':
     Log.StartLogging(sys.stdout)
-    factory = ClientFactory('zyx')
+    factory = ClientFactory('zyx', 'localhost')
 
-    reactor.connectTCP('localhost', config.BR_CLIENT_LISTEN_PORT, factory)
+    connector = reactor.connectTCP(factory.remoteHostName, config.BR_CLIENT_LISTEN_PORT, factory)
+    connector.remoteHostName = factory.remoteHostName
+
     reactor.run()
--- client_manager.py
+++ client_manager.py
@@ -2,6 +2,7 @@ from twisted.internet import reactor, protocol
 from twisted.python import log
 import sys
 import config
+from util import Log
 
 """ This is for the management of clients
 
@@ -11,31 +12,31 @@ Probably will merge PeerManager and ClientManager into one Broker class
 
 class ClientConnection(protocol.Protocol):
     def connectionMade(self):
-        log.msg('connectionMade')
+        #log.msg('connectionMade')
         self.clientManager = self.factory.clientManager
         self.clientName = 'Unknown'
+        self.Send('NREQ')
     
     def dataReceived(self, data):
-        log.msg('dataRecieved: ' + data)
+        #log.msg('dataRecieved: ' + data)
         data = data.strip()
         self.clientManager.Process(data, self)
 
     def Send(self, data):
-        log.msg('Sending data to ' + self.clientName)
+        #log.msg('Sending data to ' + self.clientName)
         self.transport.write(data)
 
     def LoseConnection(self, reason):
-        log.msg('Closing connection. Reason: ' + reason)
+        Log.Msg('[closing connection]', reason)
         self.transport.loseConnection()
 
     def connectionLost(self, reason):
         """ Just for debug use
         Log this event, do nother else
         """
-        log.msg('connectionLost for ' + self.clientName)
-
-        #if self.clientName != 'Unknown':
-            #self.clientManager.Unregister(self.clientName)
+        Log.Msg('[Connection Lost]', '[host]', self.clientName)
+        if self.clientName != 'Unknown':
+            self.clientManager.Unregister(self.clientName)
 
 class ClientConnectionFactory(protocol.ServerFactory):
     protocol = ClientConnection
@@ -51,7 +52,6 @@ class ClientManager:
     The client needs to indicate its name to the manager to 
     1. register subscriptions
     2. pull messages
-
     Each side can close the connection by sending a 'TERM' message
     """
 
@@ -59,24 +59,30 @@ class ClientManager:
         self.clients = {}   # name: connection pairs
         self.subscriptionTable = {} # name: subscription pairs
         self.listenPort = config.BR_CLIENT_LISTEN_PORT
-        
+        self.msgQueue = {} # {message_id: message}
+        self.msgDestCount = {} # {message_id: dest_count}
+        self.clientMsgQueue = {} # {client_name: message_id}
 
     def ListenTCP(self):
-        log.msg('ListenTCP')
-
+        #log.msg('ListenTCP')
         factory = ClientConnectionFactory(self)
         reactor.listenTCP(self.listenPort, factory)
 
     def Process(self, data, conn):
-        log.msg('Process data: ' + data)
+        Log.Msg('[Data Recved]', data)
+        if not ',' in data:
+            cmd, val = data, ''
+        else:
+            cmd, val = data.strip().split(',')
 
         if cmd == 'NAME':
             # register this connection
             if val is None or val == '':
-                log.msg('Error: got empty name')
+                Log.Err('[Empty Name]')
                 return
 
             conn.clientName = val
+            self.Register(conn.clientName, conn)
             return
 
         if cmd == 'MSG':
@@ -94,7 +100,6 @@ class ClientManager:
 
             for msg in msgs:
                 conn.Send('MSG,' + msg)
-
             return
 
         if cmd == 'SUB':
@@ -105,7 +110,8 @@ class ClientManager:
             conn.LoseConnection('Terminated by client')
             return
         
-        log.msg('Error: unknow command or request ' + cmd)
+        Log.Err('[unknow request]', cmd)
+        conn.LoseConnection('unknown request')
 
     def Unregister(self, name):
         """ This function is only called if this connection has been named before
@@ -117,21 +123,35 @@ class ClientManager:
         #log.msg('Unregister for client ' + name)
         #del self.clients[name]
 
-        #if self.clients.has_key(name):
-            #log.msg('Remove connection for ' + name)
-            #del self.clients[name]
+        if self.clients.has_key(name):
+            Log.Msg('[Unregisterred]', name)
+            del self.clients[name]
 
     def Register(self, name, conn):
         # this will be removed
-        log.msg('Register for client ' + name)
+        Log.Msg('[Registerred]', name)
 
         if self.clients.has_key(name):
-            log.msg('Client ' + name + ' is already registered')
-            conn.LoseConnection('Duplicate clients')
+            #Log.Err('[already registerred]', name)
+            conn.LoseConnection(name + ' already registerred')
             return
 
         conn.clientName = name
         self.clients[name] = conn
+
+    def PullAllMessage(self):
+        return self.msgQueue.values()
+
+    def PullMessage(self, client_name):
+        res = []
+        for msg_id in self.clientMsgQueue[client_name]:
+            res.append(self.msgQueue[msg_id])
+
+            self.msgDestCount[msg_id] -= 1
+            if self.msgDestCount[msg_id] <= 0:
+                del self.msgQueue[msg_id]
+
+        return res
 
 if __name__ == '__main__':
     manager = ClientManager()
