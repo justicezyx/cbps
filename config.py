5775bbe94bf6f1d5dd6fda94ddffac31da37e0a4
justicezyx <lovelybear.zhao@gmail.com>
2012-05-10 09:59:47 -0400

    Add comments to config.py

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
+# the port for accepting client connections
 BR_CLIENT_LISTEN_PORT = 10001
+
+# the port for accepting peer connections
 BR_PEER_LISTEN_PORT = 10000
+
+# the path to the peer list file on disk
 BR_PEER_LIST = 'peer_list'
-BR_CONN_TIMEOUT = 3
+
+# the timeout for expiring a connection attempt to a peering broker
+BR_CONN_TIMEOUT = 3.0
+
+# the limit of connection attempts to a peering broker
 BR_CONN_RETRY_LIMIT = 3
 
+# the path to the subscription file on disk
 BR_SUB_FILE = 'sub.data'
